33dc0eb5e4d69883dc27df45c1b96aa91b175df9
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2024-09-10 20:48:18 -0600

    Creando vistas e indexandolo a las rutas

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('about', 'about');
+Route::view('blog', 'blog');
+Route::view('contact', 'contact');
+
 // Tipos de rutas.
-Route::post();
-Route::patch();
-Route::put();
-Route::delete();
-Route::options();
+// Route::post();
+// Route::patch();
+// Route::put();
+// Route::delete();
+// Route::options();
 
 // Response a varios tipos de verbos
-Route::match(['put', 'patch'], '/', function() {
+// Route::match(['put', 'patch'], '/', function() {
 
-});
+// });
 
 // Response a todos los tipos de peticiones.
-Route::any();
+// Route::any();
